824f3232c7ff4149690f100b6dfd9515072b0226
guguoqiang2021 <84173317+guguoqiang2021@users.noreply.github.com>
2023-07-19 10:03:47 +0900

    Update triangle-opengl.c

--- examples/triangle-opengl.c
+++ examples/triangle-opengl.c
@@ -1,29 +1,5 @@
-//========================================================================
-// OpenGL triangle example
-// Copyright (c) Camilla Löwy <elmindreda@glfw.org>
-//
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would
-//    be appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not
-//    be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source
-//    distribution.
-//
-//========================================================================
-//! [code]
-
+#define _USE_MATH_DEFINES
+#
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
@@ -37,26 +13,37 @@
 
 typedef struct Vertex
 {
-    vec2 pos;
+    vec3 pos;
     vec3 col;
 } Vertex;
 
-static const Vertex vertices[3] =
+typedef enum _HitType
 {
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+    nop,
+    InTriangle,
+    OutofTriangle,
+    HitVertex1 = 10,
+    HitVertex2,
+    HitVertex3,
+
+}HitType;
+
+static Vertex vertices[3] =
+{
+    { { -0.6f, -0.4f,0.0f }, { 1.f, 0.f, 0.f } },
+    { {  0.6f, -0.4f,0.0f }, { 0.f, 1.f, 0.f } },
+    { {   0.f,  0.6f,0.0f }, { 0.f, 0.f, 1.f } }
 };
 
 static const char* vertex_shader_text =
 "#version 330\n"
 "uniform mat4 MVP;\n"
 "in vec3 vCol;\n"
-"in vec2 vPos;\n"
+"in vec3 vPos;\n"
 "out vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    gl_Position = MVP * vec4(vPos, 1.0);\n"
 "    color = vCol;\n"
 "}\n";
 
@@ -69,6 +56,14 @@ static const char* fragment_shader_text =
 "    fragment = vec4(color, 1.0);\n"
 "}\n";
 
+HitType gOperation = nop;
+
+GLfloat rotateZ = 0.f, rotateX = 0.f;
+GLfloat daltX = 0.f, daltY = 0.f;
+
+double cursorX = 0;
+double cursorY = 0;
+
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -78,6 +73,86 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button != GLFW_MOUSE_BUTTON_LEFT)
+        return;
+
+    if (action == GLFW_PRESS)
+    {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        glfwGetCursorPos(window, &cursorX, &cursorY);
+    }
+    else {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        gOperation = nop;
+    }
+}
+
+HitType Checkhit(GLFWwindow* window, int cursorX, int cursorY)
+{
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    float VirualHitX = (cursorX - width / 2.0) / width*2*1.55;
+    float VirualHitY = (-cursorY + height / 2.0) / height*2 *1.17;
+    double distance;
+    float minX=1, minY=1, maxX=-1, maxY=-1;
+    for (int iLp = 0; iLp < sizeof(vertices) / sizeof(vertices[0]); iLp++) {
+        distance = hypot(VirualHitX - vertices[iLp].pos[0], VirualHitY - vertices[iLp].pos[1]);
+        if (distance < 0.03)
+            return HitVertex1+iLp;
+
+        if (minX > vertices[iLp].pos[0])
+            minX = vertices[iLp].pos[0];
+        if (maxX < vertices[iLp].pos[0])
+            maxX = vertices[iLp].pos[0];
+        if (minY > vertices[iLp].pos[1])
+            minY = vertices[iLp].pos[1];
+        if (maxY < vertices[iLp].pos[1])
+            maxY = vertices[iLp].pos[1];
+    }
+    if (VirualHitX<maxX && VirualHitX >=minX && VirualHitY <maxY && VirualHitY >minY)
+        return InTriangle;
+    else
+        return OutofTriangle;
+}
+
+void cursor_position_callback(GLFWwindow* window, double x, double y)
+{
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+    {
+        HitType hit;
+        if (gOperation == nop) {
+            hit = Checkhit(window, cursorX, cursorY);
+            gOperation = hit;
+        }
+        else {
+            hit = gOperation;
+        }
+        switch (hit) {
+        case OutofTriangle:
+            rotateZ += (GLfloat)(x - cursorX) / 10.f;
+            rotateX += (GLfloat)(y - cursorY) / 10.f;
+            break;
+        case InTriangle:
+            daltX += (GLfloat)(x - cursorX) / 100.f;
+            daltY -= (GLfloat)(y - cursorY) / 100.f;
+            break;
+        case HitVertex1:
+        case HitVertex2:
+        case HitVertex3: {
+            GLfloat X = (GLfloat)(x - cursorX) / 100.f;
+            GLfloat Y = (GLfloat)(y - cursorY) / 100.f;
+            vertices[hit - HitVertex1].pos[0] += X;
+            vertices[hit - HitVertex1].pos[1] -= Y;
+            }
+            break;
+        }
+        cursorX = x;
+        cursorY = y;
+    }
 }
 
 int main(void)
@@ -99,18 +174,14 @@ int main(void)
     }
 
     glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
 
     glfwMakeContextCurrent(window);
     gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
 
     // NOTE: OpenGL error checks have been omitted for brevity
-
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
@@ -128,16 +199,6 @@ int main(void)
     const GLint vpos_location = glGetAttribLocation(program, "vPos");
     const GLint vcol_location = glGetAttribLocation(program, "vCol");
 
-    GLuint vertex_array;
-    glGenVertexArrays(1, &vertex_array);
-    glBindVertexArray(vertex_array);
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, pos));
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, col));
-
     while (!glfwWindowShouldClose(window))
     {
         int width, height;
@@ -147,11 +208,28 @@ int main(void)
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        mat4x4 m, p, mvp;
+        mat4x4 mvp,m,p;
         mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_translate(m, daltX, daltY, -2.0f);
+        mat4x4_rotate_X(m, m, rotateX);
+        mat4x4_rotate_Z(m, m, rotateZ);
+        mat4x4_perspective(p, 60.f * (float)M_PI / 180.f,ratio,1.f, 50.f);
         mat4x4_mul(mvp, p, m);
+
+        GLuint vertex_buffer;
+        glGenBuffers(1, &vertex_buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        GLuint vertex_array;
+        glGenVertexArrays(1, &vertex_array);
+        glBindVertexArray(vertex_array);
+        glEnableVertexAttribArray(vpos_location);
+        glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+            sizeof(Vertex), (void*)offsetof(Vertex, pos));
+        glEnableVertexAttribArray(vcol_location);
+        glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+            sizeof(Vertex), (void*)offsetof(Vertex, col));
 
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
@@ -168,4 +246,3 @@ int main(void)
     exit(EXIT_SUCCESS);
 }
 
-//! [code]
